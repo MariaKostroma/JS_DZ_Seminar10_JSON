fd5e3f37a9be117a0ac873050dac757af2199797
Maria <kostroma-mn@yandex.ru>
2023-01-26 11:09:50 +0300

    Add data.js

---
+++ data.js
@@ -0,0 +1,42 @@
+const dataInfo = `[
+{
+    "id": "ab1",
+    "url": "https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png",
+    "width": 450,
+    "height": 450,
+    "description": "Рыжий пушистик",
+    "name": "Котик 1"
+},
+{
+    "id": "ab2",
+    "url": "https://oir.mobi/uploads/posts/2021-04/1619619348_59-oir_mobi-p-samie-milie-kotiki-zhivotnie-krasivo-foto-65.jpg",
+    "width": 450,
+    "height": 450,
+    "description": "Всем привет!",
+    "name": "Котик 2"
+},
+{
+    "id": "ab3",
+    "url": "https://klike.net/uploads/posts/2018-10/1539499416_1.jpg",
+    "width": 450,
+    "height": 450,
+    "description": "Котики любят куда-нибудь залезть",
+    "name": "Котик 3"
+},
+{
+    "id": "ab4",
+    "url": "https://klike.net/uploads/posts/2018-10/1539499405_3.jpg",
+    "width": 450,
+    "height": 450,
+    "description": "Вот это глаза!",
+    "name": "Котик 4"
+},
+{
+    "id": "ab5",
+    "url": "https://klike.net/uploads/posts/2018-10/1539499424_6.jpg",
+    "width": 450,
+    "height": 450,
+    "description": "Наверное это любовь",
+    "name": "Котик 5"
+}
+]`
